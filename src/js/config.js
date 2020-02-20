dec39595aa30a5d7a646190ec088409660027ea7
Kelvinator07 <kelvinator4leo@gmail.com>
2020-02-20 12:19:53 +0100

    App Configuration

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const base_url_spoonacular = 'https://api.spoonacular.com/recipes/search?apiKey';
+export const base_url_food2fork = 'https://forkify-api.herokuapp.com/api/';
+
+export const food2fork_key = '462b1cc8d4f2730081462fbc65136320';
+export const food2fork_proxy = 'https://cors-anywhere.herokuapp.com/';
+
+export const spoonacular_key = '451a31d781f94333aa0b0c353df14fb9';
